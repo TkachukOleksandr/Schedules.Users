5ba087709ce74517314ee23a6323ea91edc3758d
TkachukSasha <sashatkachuk09@ukr.net>
2025-08-29 13:34:21 +0300

    feat: setup entities & domain events

---
+++ src/Schedules.Users.Core/Entities/Permission.cs
@@ -0,0 +1,10 @@
+﻿
+using Schedules.Nuget.Abstractions;
+
+namespace Schedules.Users.Core.Entities;
+
+public sealed class Permission : ITenantEntity
+{
+    public required string Code { get; init; }
+    public Guid? TenantId { get; set; }
+}
---
+++ src/Schedules.Users.Core/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Schedules.Nuget.Abstractions;
+using Schedules.Nuget.Enums;
+
+namespace Schedules.Users.Core.Entities;
+
+public sealed class User : ITenantEntity, ISoftDeleteEntity
+{
+    private readonly List<Role> _roles = [];
+
+    public required Guid Id { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public string? MiddleName { get; init; }
+    public required string Email { get; init; }
+    public DateTime? BirthDate { get; init; }
+    public SexType SexType { get; init; }
+    public string? IdentityId { get; set; }
+
+    public Guid? TenantId { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedOnUtc { get; set; }
+
+    public IReadOnlyCollection<Role> Roles => _roles;
+}
